43ef7ac22c7409ff10755346409371737a4e4883
rrohan-R <rrohanr4d@gmail.com>
2026-08-08 19:07:58 +0530

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int resLen = 0;
+        String res = "";
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j<n; j++){
+                if(s.charAt(i) == s.charAt(j) && (j-i <= 2 || dp[i+1][j-1])){
+                    dp[i][j] = true;
+
+                    if(resLen < j-i+1){
+                        resLen = j-i+1;
+                        res = s.substring(i, j+1);
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
